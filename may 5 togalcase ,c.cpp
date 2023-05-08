df083705b7f7c168037d818b42ca7f060c76fc11
DakshSutariya <130463070+DakshSutariya@users.noreply.github.com>
2023-05-08 17:51:44 +0530

    Add files via upload

---
+++ may 5 togalcase ,c.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main()
+{
+	char a[100]={'d','S','s','S','U','T','A','R','I','Y','A'};
+	
+	int i;
+	
+   printf("%c",a[0]-32);
+   printf("%c ",a[1]+32);
+   printf("%c",a[2]-32);
+   printf("%c",a[3]+32);
+   printf("%c",a[4]+32);
+   printf("%c",a[5]+32);
+   printf("%c",a[6]+32);
+   printf("%c",a[7]+32);
+   printf("%c",a[8]+32);
+   printf("%c",a[9]+32);
+   printf("%c",a[10]+32);
+  
+    
+}
